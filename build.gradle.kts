7e6f40eb659b9a4125985e046e1a072e928ed54b
Kelvin Law <kailaw@gmail.com>
2022-02-22 00:55:42 +0800

    build: workaround github stupid ENV variables capitalisation

--- build.gradle.kts
+++ build.gradle.kts
@@ -158,9 +158,18 @@ publishing {
 }
 
 signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    val signingKeyId: String? by project
+    val signingKey: String? = listOf(
+        project.properties["signing.secretKeyRingFile"]?.toString(),
+        System.getenv("ORG_GRADLE_PROJECT_SIGNINGKEY")?.toString(),
+    ).firstOrNull()
+    val signingPassword: String? = listOf(
+        project.properties["signing.password"]?.toString(),
+        System.getenv("ORG_GRADLE_PROJECT_SIGNINGPASSWORD")?.toString(),
+    ).firstOrNull()
+    val signingKeyId: String? = listOf(
+        project.properties["signing.keyId"]?.toString(),
+        System.getenv("ORG_GRADLE_PROJECT_SIGNINGKEYID")?.toString(),
+    ).firstOrNull()
     if (signingKey != null && signingPassword != null) {
         if (signingKeyId != null) {
             useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
